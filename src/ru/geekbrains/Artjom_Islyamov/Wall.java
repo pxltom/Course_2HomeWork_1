050e7572b286491d7b3ddd43fa89eb5d8ef4a3f8
pxltom1 <pixeltom@yandex.ru>
2021-10-24 19:25:02 +0300

    done

---
+++ src/ru/geekbrains/Artjom_Islyamov/Wall.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.Artjom_Islyamov;
+
+public class Wall implements Obstacle {
+    private final int height;
+
+    Wall(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public boolean contest(Contestant contestant) {
+        if (contestant.jump(height)) {
+            System.out.println(contestant + " перепрыгнул стену!");
+            return true;
+        } else {
+            System.out.println(contestant + " не смог перепрыгнуть стену!");
+            return false;
+        }
+    }
+}
